6a72afe5c56694af76e88b9d0b7826bbf59cee60
CoryKoehler <cjiek9181@gmail.com>
2014-07-30 13:55:26 -0400

    changed connection string to localhost
    configured webconfig and redid data model to support local host

--- GC_Project_Recipes/GC_Project_Recipes/Models/CookBookModel.Designer.cs
+++ GC_Project_Recipes/GC_Project_Recipes/Models/CookBookModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Grand\Documents\GitHub\GCTeamProject\GC_Project_Recipes\GC_Project_Recipes\Models\RecipeProjectDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Grand\Documents\GitHub\GCTeamProject\GC_Project_Recipes\GC_Project_Recipes\Models\CookBookModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
